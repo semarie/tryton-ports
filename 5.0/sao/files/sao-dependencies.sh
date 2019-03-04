aaa48dff627c6474eb0ec7f85d2405f8f2d925cd
Sebastien Marie <semarie@online.fr>
2019-03-04 19:38:40 +0100

    sao: update mousetrap dependency to 1.6.3

--- 5.0/sao/files/sao-dependencies.sh
+++ 5.0/sao/files/sao-dependencies.sh
@@ -204,7 +204,7 @@ fullcalendar			'3.10.0'	# ^3.0
 gettext_js			'0.5.5'		# ^0.5
 jquery				'3.3.1'		# ^3
 moment				'2.24.0'	# ^2.10
-mousetrap			'1.6.2'		# ^1.6
+mousetrap			'1.6.3'		# ^1.6
 papaparse			'4.6.3'		# ^4.1
 
 # cleanup (ensure it is empty)
