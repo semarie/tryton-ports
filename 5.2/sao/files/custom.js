91b6ad9ccb86b029e0835435979e94d5602ca411
Sebastien Marie <semarie@online.fr>
2019-05-09 13:53:11 +0200

    5.2: cvs sync

--- 5.2/sao/files/custom.js
+++ 5.2/sao/files/custom.js
@@ -1,4 +1,4 @@
-/* $OpenBSD: custom.js,v 1.1.1.1 2018/12/03 12:28:14 semarie Exp $ */
+/* $OpenBSD: custom.js,v 1.1.1.1 2019/05/09 11:49:00 semarie Exp $ */
 /* 
  * use this file to add javascript in order to customize sao, like changing
  * Sao.config values, inject a plugin or implement a custom widget.
